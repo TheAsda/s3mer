62eb8ec6500f7e79c5122b83c8e90c2910693706
Andrey Kiselev <omega-faworit@yandex.ru>
2021-05-28 21:55:55 +0300

    feat: add socket

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import { fastify } from 'fastify';
 import fastifyStatic from 'fastify-static';
 import { join } from 'path';
+import fastifySocket from 'fastify-socket.io';
+import { Server } from 'socket.io';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const app = fastify();
@@ -14,8 +16,19 @@ if (!isDev) {
   });
 }
 
-app.get('/kek', (reqest, reply) => {
-  reply.send('Hi');
+app.register(fastifySocket, {});
+
+app.get('/connect', (request, reply) => {});
+
+app.ready(() => {
+  (app.io as Server).on('connection', (socket) => {
+    socket.on('hostRoom', (streamerId: string) => {
+      console.log(`Hosting room with id ${streamerId}`);
+    });
+    socket.on('joinRoom', (streamerId: string, viewerId: string) => {
+      console.log(`Viewer ${viewerId} joining room ${streamerId}`);
+    });
+  });
 });
 
 app.listen(process.env.PORT ?? 9000, (err, address) =>
