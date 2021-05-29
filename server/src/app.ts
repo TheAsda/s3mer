6337fd3ec6bf1902d5ff4bece8be50f2d0c88360
Andrey Kiselev <omega-faworit@yandex.ru>
2021-05-29 13:20:40 +0300

    feat: add server web rtc support

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,15 @@
 import { fastify } from 'fastify';
 import fastifyStatic from 'fastify-static';
 import { join } from 'path';
-import fastifySocket from 'fastify-socket.io';
-import { Server } from 'socket.io';
+import fastifyCors from 'fastify-cors';
+// @ts-ignore
+import webrtc from 'wrtc';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const app = fastify();
 
 if (!isDev) {
+  console.log('Running in production');
   app.register(fastifyStatic, {
     root: join(__dirname, '..', 'build', 'client'),
   });
@@ -15,19 +17,53 @@ if (!isDev) {
     reply.sendFile('index.html');
   });
 }
+if (isDev) {
+  console.log('Running in development');
+  app.register(fastifyCors);
+}
 
-app.register(fastifySocket, {});
+const streams: Record<string, MediaStream> = {};
 
-app.get('/connect', (request, reply) => {});
+app.post('/host', async (request, reply) => {
+  console.log('Creating host');
+  const peer: RTCPeerConnection = new webrtc.RTCPeerConnection({
+    iceServers: [
+      {
+        urls: 'stun:stun.stunprotocol.org',
+      },
+    ],
+  });
+  peer.ontrack = (e) =>
+    (streams[(request.body as any).streamerId] = e.streams[0]);
+  const desc = new webrtc.RTCSessionDescription((request.body as any).sdp);
+  await peer.setRemoteDescription(desc);
+  const answer = await peer.createAnswer();
+  await peer.setLocalDescription(answer);
+  reply.send({
+    sdp: peer.localDescription,
+  });
+});
 
-app.ready(() => {
-  (app.io as Server).on('connection', (socket) => {
-    socket.on('hostRoom', (streamerId: string) => {
-      console.log(`Hosting room with id ${streamerId}`);
-    });
-    socket.on('joinRoom', (streamerId: string, viewerId: string) => {
-      console.log(`Viewer ${viewerId} joining room ${streamerId}`);
-    });
+app.post('/join', async (request, reply) => {
+  console.log('Joining room');
+  const peer = new webrtc.RTCPeerConnection({
+    iceServers: [
+      {
+        urls: 'stun:stun.stunprotocol.org',
+      },
+    ],
+  });
+  const desc = new webrtc.RTCSessionDescription((request.body as any).sdp);
+  await peer.setRemoteDescription(desc);
+  streams[(request.body as any).streamerId]
+    .getTracks()
+    .forEach((track) =>
+      peer.addTrack(track, streams[(request.body as any).streamerId])
+    );
+  const answer = await peer.createAnswer();
+  await peer.setLocalDescription(answer);
+  reply.send({
+    sdp: peer.localDescription,
   });
 });
 
