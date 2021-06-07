8e89e675f497fd3be337a715e53b35eb7a311166
Andrey Kiselev <omega-faworit@yandex.ru>
2021-06-08 00:19:22 +0300

    feat: update button styles

--- client/src/components/button/button.tsx
+++ client/src/components/button/button.tsx
@@ -4,9 +4,9 @@ import cx from 'classnames';
 export type ButtonVariantType = 'primary' | 'secondary' | 'accent';
 
 const buttonVariants: Record<ButtonVariantType, string> = {
-  primary: 'bg-blue-400',
-  secondary: 'bg-gray-400',
-  accent: 'bg-pink-400',
+  primary: 'text-paris-white bg-cello hover:bg-cello-400',  
+  secondary: 'text-cello border border-cello hover:bg-casper-100',
+  accent: 'text-paris-white bg-jelly-bean hover:bg-jelly-bean-400',
 };
 
 export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
@@ -17,7 +17,11 @@ export const Button = ({ variant, ...props }: ButtonProps) => {
   return (
     <button
       {...props}
-      className={cx(buttonVariants[variant ?? 'primary'], 'p-2 rounded-xl', props.className)}
+      className={cx(
+        buttonVariants[variant ?? 'primary'],
+        'p-2 rounded-xl focus:outline-none focus:ring focus:ring-offset-jelly-bean-400',
+        props.className
+      )}
     />
   );
 };
--- client/src/stories/Button.stories.tsx
+++ client/src/stories/Button.stories.tsx
@@ -6,13 +6,14 @@ export default {
   title: 'Button',
   component: Button,
 } as Meta;
+//https://colors.dopely.top/palette-generator/I14l1eNlL7a
 
 export const Primary = () => {
   return (
     <div className="flex gap-4">
       <Button variant="primary">Primary</Button>
       <Button variant="secondary">Secondary</Button>
-      <Button variant="accent">accent</Button>
+      <Button variant="accent">Accent</Button>
     </div>
   );
 };
