c5aa68cd18b320bfa4a792acc2b7ae3d2f521e96
Andrey Kiselev <omega-faworit@yandex.ru>
2021-06-19 13:52:34 +0300

    feat: save theme to local storage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,10 @@ import { Header } from './components/header/header';
 import { Stream } from './routes/stream/stream';
 import { Welcome } from './routes/welcome/welcome';
 import cx from 'classnames';
+import { useLocalStorage } from './lib/useLocalStorage';
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useLocalStorage('isDark', false);
   return (
     <div
       className={cx(
